4a4390828b7f6662c85e896ff652cc3bab0399fd
ahmjw <rawndummy@gmail.com>
2018-01-09 02:14:41 +0900

    Add XPath class

---
+++ src/Introvesia/PhpDomView/DomXPath.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Japan, Gunma-ken, Maebashi-shi, January 8th 2018
+ * @link http://chupoo.introvesia.com
+ * @author Ahmad <rawndummy@gmail.com>
+ */
+namespace Introvesia\PhpDomView;
+
+class DomXPath
+{
+}
